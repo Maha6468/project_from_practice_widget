4da1e774d07057774c6cf67dbf7ed4312bf634a5
Maha6468 <motherma7633@gmail.com>
2025-08-27 22:16:17 +0600

    ..

--- lib/notice/cusScaffold.dart
+++ lib/notice/cusScaffold.dart
@@ -8,7 +8,32 @@ class MScaffold extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(11.0),
+            child: TextFormField(
+              decoration:InputDecoration(
+                border:OutlineInputBorder(borderRadius: BorderRadius.circular(50)),
+                labelText: "Enter your name",
+                hintText:"Name",
 
+              )
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(11.0),
+            child: TextFormField(
+                decoration:InputDecoration(
+                  border:OutlineInputBorder(borderRadius: BorderRadius.circular(50)),
+                  labelText: "Enter your batch",
+                  hintText:"Batch",
+
+                )
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
