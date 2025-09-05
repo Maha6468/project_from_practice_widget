b09300761e81459d550af8d9f6d6ee35b45195f0
Maha6468 <motherma7633@gmail.com>
2025-09-05 23:51:13 +0600

    ..

--- lib/notice/page/secondpage.dart
+++ lib/notice/page/secondpage.dart
@@ -1,9 +1,44 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:developer';
 
-class SecondPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class SecondPage extends StatefulWidget {
   const SecondPage({super.key});
 
   @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  List users=[];
+
+  bool isloading=false;
+  Future<void> fatchUsers()async {
+    setState(() {
+      isloading=true;
+    });
+    final response=await http.get(Uri.parse("https://jsonplaceholder.typicode.com/users"));
+    setState(() {
+      isloading=false;
+    });
+    log(response.body);
+
+    if(response.statusCode==200){
+      users=jsonDecode(response.body);
+    }else{
+      throw Exception("Something worng");
+    }
+  }
+
+  @override
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fatchUsers();
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -11,7 +46,29 @@ class SecondPage extends StatelessWidget {
         backgroundColor: Colors.cyan,
         centerTitle: true,
       ),
-      
+      body:ListView.builder(
+        itemCount: users.length,
+          itemBuilder: (context,index){
+          final user=users[index];
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.purpleAccent,
+                child: Text(user["name"][0],style: TextStyle(color: Colors.white),),
+              ),
+              title: Text(user['name']),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 10,),
+                  Text("Username:${user["username"]}",style: TextStyle(color: Colors.grey),),
+                  Text("Email:${user['email']}",style: TextStyle(color: Colors.grey),),
+                  Text("Phone:${user["phone"]}",style: TextStyle(color: Colors.grey),),
+                  Text("Website:${user["website"]}",style: TextStyle(color: Colors.grey),),
+                  Text("Address:${user["address"]["street"]},${user["address"]["suite"]},",style: TextStyle(color: Colors.grey),),
+                ],
+              ),
+            );
+          })
     );
   }
 }
