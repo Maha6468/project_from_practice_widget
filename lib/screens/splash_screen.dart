3b8cd9c7ee0212395371916ac69cdc0e0f7fa4ae
Maha6468 <motherma7633@gmail.com>
2026-03-24 18:42:17 +0600

    ..last update

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -4,17 +4,14 @@ import 'package:flutter_news_app/screens/sign_in_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
-
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
-
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(
-      const Duration(seconds: 10),
+    Timer(const Duration(seconds: 10),
           () => Navigator.of(context).pushReplacement(
         MaterialPageRoute(
           builder: (BuildContext context) => const SignInScreen(),
